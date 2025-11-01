6743e40c31a8aef8a8f7ab62c795b52a42cbc8d4
Isra Kuci <israkuci20@gmail.com>
2025-11-02 00:07:40 +0100

    fix visuals and page layout

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "../index.css";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", backgroundColor: "#282c34" }}>
-      <Link to="/" style={{ color: "white", marginRight: "20px" }}>Home</Link>
-      <Link to="/publisher" style={{ color: "white", marginRight: "20px" }}>Publisher</Link>
-      <Link to="/advertiser" style={{ color: "white", marginRight: "20px" }}>Advertiser</Link>
-      <Link to="/bidding" style={{ color: "white" }}>Bidding</Link>
+    <nav className="navbar">
+      <div className="navbar-logo"> AdVenture </div>
+      <div className="navbar-links">
+        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+          Home
+        </NavLink>
+        <NavLink
+          to="/publisher"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Publisher
+        </NavLink>
+        <NavLink
+          to="/advertiser"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Advertiser
+        </NavLink>
+        <NavLink
+          to="/bidding"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Bidding
+        </NavLink>
+      </div>
     </nav>
   );
 }
